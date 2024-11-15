ebc6ca358485f785ec59d869521a0ff665858cc4
Victor Mak <contact@htetaung.com>
2024-11-15 21:26:50 +0700

    feat: Player Info Contract Struct Definition

--- src/lib.rs
+++ src/lib.rs
@@ -7,44 +7,31 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
+use alloy_primitives::Address;
 /// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::StorageString};
+use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::{StorageAddress, StorageBool, StorageMap, StorageString, StorageU64}};
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
-    }
+#[storage]
+pub struct PlayerInfo{
+    display_name: StorageString,
+    winning_matches: StorageU64,
+    total_matches: StorageU64,
+    winning_predictions: StorageU64,
+    total_predictions: StorageU64
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
+#[storage]
+#[entrypoint]
+pub struct PlayerInfoContract{
+    initialized: StorageBool,
+    owner: StorageAddress,
+    matchmaking_contract: StorageAddress,
+    prediction_contract: StorageAddress,
+    player_info: StorageMap<Address, PlayerInfo>
+}
+
 #[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
-    }
+impl PlayerInfoContract{
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
-    }
 }
+
